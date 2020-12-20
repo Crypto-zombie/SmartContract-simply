d0047f902e9704d760b107726fda7c62d526f49b
cano <valcano103@gmail.com>
2020-12-21 00:00:00 +0200

    Update

---
+++ helper.js
@@ -0,0 +1,528 @@
+Contribution: 2020-12-16 00:00
+
+Contribution: 2020-12-16 00:01
+
+Contribution: 2020-12-16 00:02
+
+Contribution: 2020-12-16 00:03
+
+Contribution: 2020-12-16 00:04
+
+Contribution: 2020-12-17 00:00
+
+Contribution: 2020-12-16 00:00
+
+Contribution: 2020-12-16 00:01
+
+Contribution: 2020-12-17 00:00
+
+Contribution: 2020-12-17 00:01
+
+Contribution: 2020-12-17 00:02
+
+Contribution: 2020-12-17 00:03
+
+Contribution: 2020-12-25 00:00
+
+Contribution: 2020-12-25 00:01
+
+Contribution: 2020-12-25 00:02
+
+Contribution: 2020-12-27 00:00
+
+Contribution: 2020-12-28 00:00
+
+Contribution: 2020-12-31 00:00
+
+Contribution: 2020-12-31 00:01
+
+Contribution: 2021-01-01 00:00
+
+Contribution: 2021-01-01 00:01
+
+Contribution: 2021-01-01 00:02
+
+Contribution: 2021-01-01 00:03
+
+Contribution: 2021-01-04 00:00
+
+Contribution: 2021-01-04 00:01
+
+Contribution: 2021-01-04 00:02
+
+Contribution: 2021-01-04 00:03
+
+Contribution: 2021-01-04 00:04
+
+Contribution: 2021-01-06 00:00
+
+Contribution: 2021-01-06 00:01
+
+Contribution: 2021-01-16 00:00
+
+Contribution: 2021-01-16 00:01
+
+Contribution: 2021-01-16 00:02
+
+Contribution: 2021-01-16 00:03
+
+Contribution: 2021-01-16 00:04
+
+Contribution: 2021-01-16 00:05
+
+Contribution: 2021-01-18 00:00
+
+Contribution: 2021-01-18 00:01
+
+Contribution: 2021-01-22 00:00
+
+Contribution: 2021-01-22 00:01
+
+Contribution: 2020-12-16 00:00
+
+Contribution: 2020-12-16 00:01
+
+Contribution: 2020-12-20 00:00
+
+Contribution: 2020-12-20 00:01
+
+Contribution: 2020-12-20 00:02
+
+Contribution: 2020-12-22 00:00
+
+Contribution: 2020-12-22 00:01
+
+Contribution: 2020-12-30 00:00
+
+Contribution: 2020-12-30 00:01
+
+Contribution: 2021-01-01 00:00
+
+Contribution: 2021-01-02 00:00
+
+Contribution: 2021-01-02 00:01
+
+Contribution: 2021-01-02 00:02
+
+Contribution: 2021-01-02 00:03
+
+Contribution: 2021-01-05 00:00
+
+Contribution: 2021-01-06 00:00
+
+Contribution: 2021-01-06 00:01
+
+Contribution: 2021-01-06 00:02
+
+Contribution: 2021-01-06 00:03
+
+Contribution: 2021-01-08 00:00
+
+Contribution: 2021-01-09 00:00
+
+Contribution: 2021-01-09 00:01
+
+Contribution: 2021-01-09 00:02
+
+Contribution: 2021-01-09 00:03
+
+Contribution: 2021-01-09 00:04
+
+Contribution: 2021-01-09 00:05
+
+Contribution: 2021-01-11 00:00
+
+Contribution: 2021-01-12 00:00
+
+Contribution: 2021-01-12 00:01
+
+Contribution: 2021-01-12 00:02
+
+Contribution: 2021-01-12 00:03
+
+Contribution: 2021-01-12 00:04
+
+Contribution: 2021-01-13 00:00
+
+Contribution: 2021-01-13 00:01
+
+Contribution: 2021-01-13 00:02
+
+Contribution: 2021-01-13 00:03
+
+Contribution: 2021-01-13 00:04
+
+Contribution: 2021-01-13 00:05
+
+Contribution: 2021-01-15 00:00
+
+Contribution: 2021-01-15 00:01
+
+Contribution: 2021-01-15 00:02
+
+Contribution: 2021-01-15 00:03
+
+Contribution: 2021-01-15 00:04
+
+Contribution: 2021-01-15 00:05
+
+Contribution: 2021-01-20 00:00
+
+Contribution: 2021-01-20 00:01
+
+Contribution: 2021-01-20 00:02
+
+Contribution: 2021-01-20 00:03
+
+Contribution: 2021-01-20 00:04
+
+Contribution: 2021-01-20 00:05
+
+Contribution: 2021-01-21 00:00
+
+Contribution: 2021-01-21 00:01
+
+Contribution: 2021-01-21 00:02
+
+Contribution: 2021-01-21 00:03
+
+Contribution: 2021-01-22 00:00
+
+Contribution: 2020-12-17 00:00
+
+Contribution: 2020-12-17 00:01
+
+Contribution: 2020-12-17 00:02
+
+Contribution: 2020-12-17 00:03
+
+Contribution: 2020-12-17 00:04
+
+Contribution: 2020-12-17 00:05
+
+Contribution: 2020-12-19 00:00
+
+Contribution: 2020-12-19 00:01
+
+Contribution: 2020-12-19 00:02
+
+Contribution: 2020-12-29 00:00
+
+Contribution: 2020-12-29 00:01
+
+Contribution: 2021-01-01 00:00
+
+Contribution: 2021-01-02 00:00
+
+Contribution: 2021-01-07 00:00
+
+Contribution: 2021-01-07 00:01
+
+Contribution: 2021-01-09 00:00
+
+Contribution: 2021-01-09 00:01
+
+Contribution: 2021-01-12 00:00
+
+Contribution: 2021-01-17 00:00
+
+Contribution: 2020-12-16 00:00
+
+Contribution: 2020-12-16 00:01
+
+Contribution: 2020-12-16 00:02
+
+Contribution: 2020-12-17 00:00
+
+Contribution: 2020-12-19 00:00
+
+Contribution: 2020-12-19 00:01
+
+Contribution: 2020-12-19 00:02
+
+Contribution: 2020-12-19 00:03
+
+Contribution: 2020-12-19 00:04
+
+Contribution: 2020-12-20 00:00
+
+Contribution: 2020-12-22 00:00
+
+Contribution: 2020-12-22 00:01
+
+Contribution: 2020-12-22 00:02
+
+Contribution: 2020-12-22 00:03
+
+Contribution: 2020-12-23 00:00
+
+Contribution: 2020-12-23 00:01
+
+Contribution: 2020-12-23 00:02
+
+Contribution: 2020-12-28 00:00
+
+Contribution: 2020-12-28 00:01
+
+Contribution: 2020-12-28 00:02
+
+Contribution: 2021-01-04 00:00
+
+Contribution: 2021-01-04 00:01
+
+Contribution: 2021-01-06 00:00
+
+Contribution: 2021-01-06 00:01
+
+Contribution: 2021-01-06 00:02
+
+Contribution: 2021-01-06 00:03
+
+Contribution: 2021-01-07 00:00
+
+Contribution: 2021-01-08 00:00
+
+Contribution: 2021-01-08 00:01
+
+Contribution: 2021-01-08 00:02
+
+Contribution: 2021-01-08 00:03
+
+Contribution: 2021-01-08 00:04
+
+Contribution: 2021-01-09 00:00
+
+Contribution: 2021-01-12 00:00
+
+Contribution: 2021-01-12 00:01
+
+Contribution: 2021-01-12 00:02
+
+Contribution: 2021-01-12 00:03
+
+Contribution: 2021-01-12 00:04
+
+Contribution: 2021-01-12 00:05
+
+Contribution: 2021-01-14 00:00
+
+Contribution: 2021-01-15 00:00
+
+Contribution: 2021-01-15 00:01
+
+Contribution: 2021-01-15 00:02
+
+Contribution: 2021-01-15 00:03
+
+Contribution: 2021-01-17 00:00
+
+Contribution: 2021-01-17 00:01
+
+Contribution: 2021-01-17 00:02
+
+Contribution: 2021-01-17 00:03
+
+Contribution: 2021-01-19 00:00
+
+Contribution: 2021-01-19 00:01
+
+Contribution: 2021-01-19 00:02
+
+Contribution: 2021-01-19 00:03
+
+Contribution: 2021-01-19 00:04
+
+Contribution: 2021-01-21 00:00
+
+Contribution: 2021-01-21 00:01
+
+Contribution: 2021-01-21 00:02
+
+Contribution: 2020-12-17 00:00
+
+Contribution: 2020-12-17 00:01
+
+Contribution: 2020-12-17 00:02
+
+Contribution: 2020-12-20 00:00
+
+Contribution: 2020-12-20 00:01
+
+Contribution: 2020-12-20 00:02
+
+Contribution: 2020-12-20 00:03
+
+Contribution: 2020-12-20 00:04
+
+Contribution: 2020-12-20 00:05
+
+Contribution: 2020-12-23 00:00
+
+Contribution: 2020-12-23 00:01
+
+Contribution: 2020-12-23 00:02
+
+Contribution: 2020-12-23 00:03
+
+Contribution: 2020-12-24 00:00
+
+Contribution: 2020-12-24 00:01
+
+Contribution: 2020-12-24 00:02
+
+Contribution: 2020-12-24 00:03
+
+Contribution: 2020-12-24 00:04
+
+Contribution: 2020-12-26 00:00
+
+Contribution: 2020-12-31 00:00
+
+Contribution: 2021-01-02 00:00
+
+Contribution: 2021-01-02 00:01
+
+Contribution: 2021-01-04 00:00
+
+Contribution: 2021-01-04 00:01
+
+Contribution: 2021-01-04 00:02
+
+Contribution: 2021-01-04 00:03
+
+Contribution: 2021-01-09 00:00
+
+Contribution: 2021-01-09 00:01
+
+Contribution: 2021-01-09 00:02
+
+Contribution: 2021-01-09 00:03
+
+Contribution: 2021-01-09 00:04
+
+Contribution: 2021-01-10 00:00
+
+Contribution: 2021-01-10 00:01
+
+Contribution: 2021-01-10 00:02
+
+Contribution: 2021-01-10 00:03
+
+Contribution: 2021-01-10 00:04
+
+Contribution: 2021-01-11 00:00
+
+Contribution: 2021-01-11 00:01
+
+Contribution: 2021-01-11 00:02
+
+Contribution: 2021-01-11 00:03
+
+Contribution: 2021-01-11 00:04
+
+Contribution: 2021-01-11 00:05
+
+Contribution: 2021-01-13 00:00
+
+Contribution: 2021-01-17 00:00
+
+Contribution: 2021-01-17 00:01
+
+Contribution: 2021-01-17 00:02
+
+Contribution: 2021-01-17 00:03
+
+Contribution: 2020-12-22 00:00
+
+Contribution: 2020-12-22 00:01
+
+Contribution: 2020-12-22 00:02
+
+Contribution: 2020-12-22 00:03
+
+Contribution: 2020-12-22 00:04
+
+Contribution: 2020-12-24 00:00
+
+Contribution: 2020-12-24 00:01
+
+Contribution: 2020-12-24 00:02
+
+Contribution: 2020-12-24 00:03
+
+Contribution: 2020-12-24 00:04
+
+Contribution: 2020-12-27 00:00
+
+Contribution: 2020-12-27 00:01
+
+Contribution: 2020-12-29 00:00
+
+Contribution: 2020-12-30 00:00
+
+Contribution: 2020-12-30 00:01
+
+Contribution: 2021-01-04 00:00
+
+Contribution: 2021-01-05 00:00
+
+Contribution: 2021-01-05 00:01
+
+Contribution: 2021-01-05 00:02
+
+Contribution: 2021-01-05 00:03
+
+Contribution: 2021-01-06 00:00
+
+Contribution: 2021-01-06 00:01
+
+Contribution: 2021-01-06 00:02
+
+Contribution: 2021-01-06 00:03
+
+Contribution: 2021-01-06 00:04
+
+Contribution: 2021-01-06 00:05
+
+Contribution: 2021-01-13 00:00
+
+Contribution: 2021-01-13 00:01
+
+Contribution: 2021-01-13 00:02
+
+Contribution: 2021-01-13 00:03
+
+Contribution: 2021-01-13 00:04
+
+Contribution: 2021-01-14 00:00
+
+Contribution: 2021-01-16 00:00
+
+Contribution: 2021-01-16 00:01
+
+Contribution: 2021-01-16 00:02
+
+Contribution: 2021-01-16 00:03
+
+Contribution: 2021-01-16 00:04
+
+Contribution: 2021-01-16 00:05
+
+Contribution: 2021-01-17 00:00
+
+Contribution: 2021-01-17 00:01
+
+Contribution: 2021-01-17 00:02
+
+Contribution: 2021-01-20 00:00
+
+Contribution: 2021-01-20 00:01
+
+Contribution: 2021-01-20 00:02
+
+Contribution: 2021-01-20 00:03
+
+Contribution: 2021-01-20 00:04
+
+Contribution: 2020-12-21 00:00
+
